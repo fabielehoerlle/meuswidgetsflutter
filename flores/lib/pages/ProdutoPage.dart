b3594d4c46b4fe5d2ad8ded8bd804977c1e48d35
Fabiele Hoerlle <fabihoerlle@gmail.com>
2022-06-22 21:54:44 -0300

    layout da barra lateral do produto

--- flores/lib/pages/ProdutoPage.dart
+++ flores/lib/pages/ProdutoPage.dart
@@ -33,21 +33,42 @@ class ProdutoBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: const [
-          ProdutoInfo(
-              icon: Icons.local_florist_outlined,
-              text1: "Flores Frescas",
-              text2: "Padrão de Qualidade"),
-          ProdutoInfo(
-              icon: Icons.email_outlined,
-              text1: "Acompanha Cartão",
-              text2: "Exclusivo para sua Mensagem"),
-          ProdutoInfo(
-              icon: Icons.local_shipping_outlined,
-              text1: "Entregamos em Porto Alegre e Vale dos Sinos",
-              text2: "Saiba mais detalhes sobre a entrega"),
+      constraints: const BoxConstraints(maxWidth: 950),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const [
+              ProdutoInfo(
+                  icon: Icons.local_florist_outlined,
+                  text1: "Flores Frescas",
+                  text2: "Padrão de Qualidade"),
+              ProdutoInfo(
+                  icon: Icons.email_outlined,
+                  text1: "Acompanha Cartão",
+                  text2: "Exclusivo para sua Mensagem"),
+              ProdutoInfo(
+                  icon: Icons.local_shipping_outlined,
+                  text1: "Entregamos em Porto Alegre e Vale dos Sinos",
+                  text2: "Saiba mais detalhes sobre a entrega"),
+            ],
+          ),
+          Row(
+            children: [
+              Container(
+                height: 500,
+                width: 550,
+                color: Colors.grey,
+              ),
+              Column(
+                children: const [
+                  Text("Garden Roses"),
+                  Text("REF: 10605"),
+                  Text("R\$ 438,00"),
+                ],
+              ),
+            ],
+          ),
         ],
       ),
     );
@@ -68,7 +89,7 @@ class ProdutoInfo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.only(top: 50, left: 50, right: 50),
+      padding: const EdgeInsets.all(50),
       child: Row(
         children: [
           Icon(icon),
