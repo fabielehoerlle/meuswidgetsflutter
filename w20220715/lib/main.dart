965f81e8e68cd611f2fad952a5bf214bc187aeb8
Fabiele Hoerlle <fabihoerlle@gmail.com>
2022-07-19 19:36:31 -0300

    Widget de Produto de Beleza

--- w20220715/lib/main.dart
+++ w20220715/lib/main.dart
@@ -46,101 +46,166 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        child: Stack(
-          children: [
-            Container(
-              padding: const EdgeInsets.only(left: 20, right: 20),
-              height: 430,
-              width: 220,
-              decoration: BoxDecoration(
-                border: Border.all(width: 1, color: Colors.grey),
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Image.asset("assets/1.png"),
-                  const SizedBox(height: 10),
-                  Container(
-                    alignment: Alignment.center,
-                    height: 20,
-                    width: 80,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(5),
-                      border: Border.all(width: 1, color: Colors.pink),
-                    ),
-                    child: const Text(
-                      "Lançamento",
-                      style: TextStyle(
-                        fontSize: 12,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.pink,
-                      ),
-                    ),
-                  ),
-                  const SizedBox(height: 10),
-                  const Text(
-                    "Nivea",
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black,
-                    ),
-                  ),
-                  const SizedBox(height: 5),
-                  const Text(
-                    "NIVEA Soft - Creme \nHidratante 97g",
-                  ),
-                  const SizedBox(height: 5),
-                  Row(
-                    children: const [
-                      Text(
-                        "R\$ 43,90",
-                        style: TextStyle(
-                          decoration: TextDecoration.lineThrough,
-                          fontSize: 12,
-                        ),
-                      ),
-                      SizedBox(width: 5),
-                      Text(
-                        "R\$ 34,90",
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ],
-                  ),
-                  const SizedBox(height: 10),
-                  Container(
-                    width: 150,
-                    child: const Text(
-                        "Creme hidratante para corpo, mãos e rosto, " +
-                            "ideal para todos os tipos de pele. Suaviza a textura da pele."),
-                  ),
-                ],
-              ),
-            ),
-            Positioned(
-              top: 10,
-              right: 10,
-              child: Container(
-                alignment: Alignment.center,
-                height: 22,
-                width: 45,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(5),
-                  color: Colors.yellow,
-                ),
-                child: const Text(
-                  "- 20%",
-                  style: TextStyle(
-                    fontSize: 12,
-                    color: Colors.black,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
+        child: Row(
+          children: const [
+            Produtos(
+                image: "assets/1.png",
+                color: Colors.pink,
+                producttype: "Lançamento",
+                textcolor: Colors.pink,
+                productbrand: "Nivea",
+                productname: "NIVEA MEN Deep - \nShower Gel 250ml",
+                originalvalue: "R\$ 39,90",
+                discountedvalue: "R\$ 30,90",
+                text: "Shower gel para todos " +
+                    "os tipos de pele. Possui " +
+                    "ação 3 em 1, higieniza o corpo, rosto e até o cabelo.",
+                discountpercentage: "- 22%"),
+            Produtos(
+              image: "assets/2.png",
+              color: Colors.green,
+              producttype: "Vegano",
+              textcolor: Colors.green,
+              productbrand: "Quem Disse, \nBerenice?",
+              productname:
+                  "quem disse, berenice? \nGlow - Esfoliante \nCorporal 150g",
+              originalvalue: "R\$ 37,90",
+              discountedvalue: "R\$ 34,11",
+              text: "Esfoliante corporal. Ajuda a remover as impurezas " +
+                  "e células mortas para uma pele renovada e luminosa.",
+              discountpercentage: "- 10%",
             ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class Produtos extends StatelessWidget {
+  const Produtos({
+    required this.image,
+    required this.color,
+    required this.producttype,
+    required this.textcolor,
+    required this.productbrand,
+    required this.productname,
+    required this.originalvalue,
+    required this.discountedvalue,
+    required this.text,
+    required this.discountpercentage,
+    Key? key,
+  }) : super(key: key);
+  final String image;
+  final Color color;
+  final String producttype;
+  final Color textcolor;
+  final String productbrand;
+  final String productname;
+  final String originalvalue;
+  final String discountedvalue;
+  final String text;
+  final String discountpercentage;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(20),
+      child: Stack(
+        children: [
+          Container(
+            padding: const EdgeInsets.only(left: 20, right: 20),
+            height: 430,
+            width: 220,
+            decoration: BoxDecoration(
+              border: Border.all(width: 1, color: Colors.grey),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  height: 180,
+                  child: Image.asset(image),
+                ),
+                const SizedBox(height: 5),
+                Container(
+                  alignment: Alignment.center,
+                  height: 20,
+                  //width: 80,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(5),
+                    border: Border.all(width: 1, color: color),
+                  ),
+                  child: Text(
+                    producttype,
+                    style: TextStyle(
+                      fontSize: 12,
+                      fontWeight: FontWeight.bold,
+                      color: textcolor,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 10),
+                Text(
+                  productbrand,
+                  style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                  ),
+                ),
+                const SizedBox(height: 5),
+                Text(
+                  productname,
+                ),
+                const SizedBox(height: 5),
+                Row(
+                  children: [
+                    Text(
+                      originalvalue,
+                      style: const TextStyle(
+                        decoration: TextDecoration.lineThrough,
+                        fontSize: 12,
+                      ),
+                    ),
+                    const SizedBox(width: 5),
+                    Text(
+                      discountedvalue,
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 10),
+                Container(
+                  width: 150,
+                  child: Text(text),
+                ),
+              ],
+            ),
+          ),
+          Positioned(
+            top: 10,
+            right: 10,
+            child: Container(
+              alignment: Alignment.center,
+              height: 22,
+              width: 45,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(5),
+                color: Colors.yellow,
+              ),
+              child: Text(
+                discountpercentage,
+                style: const TextStyle(
+                  fontSize: 12,
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
